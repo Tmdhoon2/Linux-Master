b31f2c09812845b0f6ac39c915385dfec0aea612
Tmdhoon2 <hks026naver@gmail.com>
2023-03-24 02:46:36 +0000

    feat :: Day04

---
+++ Day04/ex04-08.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+	if(symlink(argv[1], argv[2])){
+		printf("soft-link failed");
+	}
+}
+
---
+++ Day04/ex04-09.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+
+	char buffer[1024];
+	int nread;
+	
+	nread = readlink();
+}
+
