f90bb059dcf3777472cf8c5e770d64f900e01110
Tmdhoon2 <hks026naver@gmail.com>
2023-06-16 10:23:25 +0900

    ⚡️ :: trim.h

---
+++ Linux-project/trim.h
@@ -0,0 +1,34 @@
+//
+// Created by 정승훈 on 2023/06/16.
+//
+
+#ifndef C_TRIM_H
+#define C_TRIM_H
+
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+char *ltrim(const char *s){
+    char *r = strdup(s);
+    if(r != NULL){
+        char *fr = r + strlen(s) - 1;
+        while((isspace(*fr) || !isprint(*fr) || *fr == 0) && fr >= r) --fr;
+        *++fr = 0;
+    }
+    return r;
+}
+
+char *rtrim(const char *s){
+    while(isspace(*s) || !isprint(*s)) ++s;
+    return strdup(s);
+}
+
+char *trim(const char *s){
+    char *r = rtrim(s);
+    char *f = ltrim(r);
+    free(r);
+    return f;
+}
+
+#endif
